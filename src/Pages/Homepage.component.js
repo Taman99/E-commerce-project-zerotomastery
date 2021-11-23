690c37841b2c712ea58a9e326d11603239872ca5
Taman99 <tghunia@gmail.com>
2021-11-23 14:37:02 +0530

    created homepage, menuitem and directory

--- src/Pages/Homepage.component.js
+++ src/Pages/Homepage.component.js
@@ -1,47 +1,13 @@
 import React from 'react'
+import MenuItem from '../Components/menu-item/MenuItem.component'
+import Directory from '../Components/Directory/Directory.component'
 import './Homepage.styles.scss'
 
 export default function Homepage() {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item"> 
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">shop now </span>
-                    </div>
-                </div>
-
-                <div className="menu-item"> 
-                    <div className="content">
-                        <h1 className="title">t-shirts</h1>
-                        <span className="subtitle">shop now </span>
-                    </div>
-                </div>
-
-                <div className="menu-item"> 
-                    <div className="content">
-                        <h1 className="title">Jeans</h1>
-                        <span className="subtitle">shop now </span>
-                    </div>
-                </div>
-                
-                <div className="menu-item"> 
-                    <div className="content">
-                        <h1 className="title">Pants</h1>
-                        <span className="subtitle">shop now </span>
-                    </div>
-                </div>
-                
-
-                <div className="menu-item"> 
-                    <div className="content">
-                        <h1 className="title">Trousers</h1>
-                        <span className="subtitle">shop now </span>
-                    </div>
-                </div>
-                
-            </div>            
-        </div>
+            <Directory />           
+        </div>            
+    
     )
 }
